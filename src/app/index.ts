989c42ff7f399ec13808ae426e783e9b018edd2b
Felix Orinda <forinda82@gmail.com>
2024-10-13 05:10:28 +0300

    Add test endpoint on the backend

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,14 +1,22 @@
-import express from 'express'
+import express from "express";
 
-const app = express()
+const app = express();
 
-app.get('/',(req,res)=>{
-    return res.status(200).json({message:"OK"})
-})
+app.get("/", (req, res) => {
+  return res.status(200).json({ message: "OK" });
+});
+app.get("/test", (req, res) => {
+  return res.status(200).json({
+    message: "OK",
+    data: {
+      users: [],
+      status: "OK",
+    },
+  });
+});
 
-app.all('*',(_,res)=>{
-    return res.sendStatus(405)
-})
+app.all("*", (_, res) => {
+  return res.sendStatus(405);
+});
 
-
-export default app
+export default app;
